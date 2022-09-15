d3df32d763e68ec133e1cd8065978911d9372429
romchie <rraguso10@gmail.com>
2022-09-15 15:54:29 -0400

    implemented getpass library

--- getUserPass.py
+++ getUserPass.py
@@ -1,9 +1,10 @@
 import os
 import time
+from getpass import getpass
 
 def main():
     u = input("Enter your PODS username: ")
-    p = input("Enter your PODS password: ")
+    p = getpass("Enter your PODS password: ")
     os.system("touch .certs")
     f = open(".certs", 'w')
     f.write(str(u + '\n' + p + '\n'))
